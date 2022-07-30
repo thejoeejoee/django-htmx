245f9a6a07cd81e9b1dee6772e68284aa7ad2f06
Josef Kolář <thejoeejoee@gmail.com>
2022-07-30 17:54:25 +0300

    Added support for DEBUG_PROPAGATE_EXCEPTIONS

--- src/django_htmx/jinja.py
+++ src/django_htmx/jinja.py
@@ -7,8 +7,8 @@ from django.utils.html import format_html
 
 def django_htmx_script() -> str:
     # Optimization: whilst the script has no behaviour outside of debug mode,
-    # don't include it.
-    if not settings.DEBUG:
+    # don't include it. Same if exception propagating is enabled in Django.
+    if not settings.DEBUG or settings.DEBUG_PROPAGATE_EXCEPTIONS:
         return format_html("")
     return format_html(
         '<script type="text/javascript" src="{}" data-debug="{}" defer></script>',
